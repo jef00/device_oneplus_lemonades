LineageOS/android_device_oneplus_kebab

kebab: overlay: Configure aux camera for Aperture
Change-Id: I543f078dd4dba30e1b22737d5c22d591e3fb34e6

device.mk
@@ -28,6 +28,9 @@ PRODUCT_PACKAGES += \
     fstab.qcom.ramdisk
 
 # Overlays
+DEVICE_PACKAGE_OVERLAYS += \
+    $(LOCAL_PATH)/overlay-lineage
+
 PRODUCT_PACKAGES += \
     OPlusFrameworksResTarget \
     OPlusSettingsProviderResTarget \

jef00/device_oneplus_lemonades

device.mk
@@ -28,6 +28,9 @@ PRODUCT_PACKAGES += \
     fstab.qcom.ramdisk
 
 # Overlays
+DEVICE_PACKAGE_OVERLAYS += \
+    $(LOCAL_PATH)/overlay-aosp
+
 PRODUCT_PACKAGES += \
     OPlusFrameworksResTarget \
     OPlusSettingsProviderResTarget \